9f81f17ab117a1879eab86be128be27c688c0198
Jan Layola <gilajan@proton.me>
2024-01-03 16:52:19 +0100

    Add support for triggering a function on the last card

--- src/app/components/IssueCardComponent/IssueCardComponent.tsx
+++ src/app/components/IssueCardComponent/IssueCardComponent.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from "react";
+import {MutableRefObject, ReactNode, useEffect, useRef} from "react";
 
 import styles from "./IssueCardComponent.module.css"
 export interface IssueCardComponentProps {
@@ -7,23 +7,41 @@ export interface IssueCardComponentProps {
   number: number;
   state: 'open' | 'closed' ;
   userName: any;
+  isLast: boolean;
+  newLimit: () => void;
 }
 
-const IssueCardComponent = ({ title, labels, number, state, userName }: IssueCardComponentProps): ReactNode => (
-  <div className={styles.issueCardContainer}>
-    <div className={styles.topSection}>
-      <div className={styles.row}>
-        <div className={state === 'open' ? styles.open : styles.closed}/>
-        <p className={styles.issueTitle}>{title}</p>
+const IssueCardComponent = ({ title, labels, number, state, userName, isLast = false, newLimit }: IssueCardComponentProps): ReactNode => {
+  const cardRef: MutableRefObject<any> = useRef(); // A better typing could be found fo sure, but the clock is running!
+  useEffect((): void => {
+    if (!cardRef?.current) return;
+
+    const observer = new IntersectionObserver(([entry]) => {
+      if (isLast && entry.isIntersecting) {
+        newLimit();
+        observer.unobserve(entry.target);
+      }
+    });
+
+    observer.observe(cardRef.current);
+  }, [isLast])
+
+  return (
+    <div className={styles.issueCardContainer} ref={cardRef}>
+      <div className={styles.topSection}>
+        <div className={styles.row}>
+          <div className={state === 'open' ? styles.open : styles.closed}/>
+          <p className={styles.issueTitle}>{title}</p>
+        </div>
+        <div className={styles.row}>
+          {labels.map((label) => <div className={styles.label} key={label}>{label}</div>)}
+        </div>
       </div>
-      <div className={styles.row}>
-        {labels.map((label) => <div className={styles.label} key={label}>{label}</div>)}
+      <div>
+        <p>#{number} opened 4 hours ago by {userName}</p>
       </div>
     </div>
-    <div>
-      <p>#{number} opened 4 hours ago by {userName}</p>
-    </div>
-  </div>
-)
+  )
+}
 
 export default IssueCardComponent;
